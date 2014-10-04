c3c88c4a1a3dad83164974e0e89162af6868dc41
Matthew Feldman <matthew.feldman@gmail.com>
2014-10-03 20:07:23 -0700

    removed login requirements

--- chartly/website/urls.py
+++ chartly/website/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required, permission_required
+#from django.contrib.auth.decorators import login_required, permission_required
 
 from views import *
 
 urlpatterns = patterns('',
-   	url(r'^$', login_required(index), name='home'),
+   	url(r'^$', index, name='home'),
    	url(r'^query_api/(?P<query_id>\d+)$', query_api, name='query_api'),
    	url(r'^query/(?P<query_ids>[,\w]+)$', query, name='query'),
    	url(r'^query/(?P<query_names>[\w\ ]+)$', query_name, name='query_name'),
