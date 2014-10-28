c647d67640c0b95f432aac0308068f31ff9ca8a7
Matthew Feldman <matthew.feldman@gmail.com>
2014-10-28 13:02:11 -0700

    fixed some template bugs: include edit button and hide description if not approps

--- chartly/accounts/urls.py
+++ chartly/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url, include
 urlpatterns = patterns('',
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'accounts/login.html'}, name='logout')
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'accounts:login'}, name='logout')
 )
