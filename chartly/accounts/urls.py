53211dc9aeeb9041232ca12e871dfa17a223b299
Matthew Feldman <matthew.feldman@gmail.com>
2014-10-30 19:03:01 -0700

    WOO fixed login system and added flair!

--- chartly/accounts/urls.py
+++ chartly/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, url, include
+from django.views.generic import TemplateView
+
 urlpatterns = patterns('',
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'accounts:login'}, name='logout')
+    url(r'^logout$','django.contrib.auth.views.logout', {'next_page': 'accounts:logout_confirm'}, name='logout'),
+    url(r'^logout_confirm$', TemplateView.as_view(template_name='accounts/logout.html'), name='logout_confirm')
 )
