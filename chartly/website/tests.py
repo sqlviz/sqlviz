d144062cc1d3a0db2bf447b151f8e5d169ac2f9b
Trey Hunner <trey@treyhunner.com>
2014-12-12 13:53:31 -0800

    Remove broken test

--- chartly/website/tests.py
+++ chartly/website/tests.py
@@ -1,13 +1,1 @@
 from django.test import TestCase
-import query
-# Create your tests here.
-
-class DataManagerTest(TestCase):
-
-    def test_was_published_recently_with_future_poll(self):
-        """
-        was_published_recently() should return False for polls whose
-        pub_date is in the future
-        """
-        DM = DataManager(1)
-        self.assertEqual(DM.query_id, 1)
