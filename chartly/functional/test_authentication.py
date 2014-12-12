e75427d284f4c2deb963f44e80c0f12fe5237dfd
Trey Hunner <trey@treyhunner.com>
2014-12-12 14:29:15 -0800

    Add tests for admin login page

---
+++ chartly/functional/test_authentication.py
@@ -0,0 +1,49 @@
+from django.contrib.auth.models import User
+from django.test import LiveServerTestCase
+from splinter import Browser
+
+
+class AdminLoginTest(LiveServerTestCase):
+
+    username = "username"
+    password = "password"
+
+    def setUp(self):
+        self.browser = Browser('django')
+        self.browser.visit("%s/admin/" % self.live_server_url)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def login(self):
+        self.browser.fill('username', self.username)
+        self.browser.fill('password', self.password)
+        self.browser.find_by_value('Log in').click()
+
+    def test_invalid_username(self):
+        """Ensure error shown when logging in with an invalid username."""
+        assert not self.browser.find_by_css('.errornote')
+        self.login()
+        assert "case-sensitive" in self.browser.find_by_css('.errornote').text
+
+    def test_incorrect_password(self):
+        """Ensure error shown when logging in with an incorrect password."""
+        User.objects.create_superuser(
+            username=self.username,
+            email="u@example.com",
+            password="other password",
+        )
+        assert not self.browser.find_by_css('.errornote')
+        self.login()
+        assert "case-sensitive" in self.browser.find_by_css('.errornote').text
+
+    def test_valid_credentials(self):
+        """Ensure logging in with valid credentials redirects to admin."""
+        User.objects.create_superuser(
+            username=self.username,
+            email="u@example.com",
+            password=self.password,
+        )
+        assert "Log out" not in self.browser.find_by_css('body').text
+        self.login()
+        assert "Log out" in self.browser.find_by_css('body').text
