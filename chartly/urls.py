75f53857ee1fae10b888c24d1a4fb2f7dc277f02
Matthew Feldman <matthew.feldman@gmail.com>
2014-12-28 17:24:37 -0800

    changed url mapping

--- chartly/urls.py
+++ chartly/urls.py
@@ -4,12 +4,10 @@ from django.views.generic.base import RedirectView
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^website/', include('website.urls', namespace='website')),
+    url(r'^app/', include('website.urls', namespace='website')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    #url(r'^favit/', include('favit.urls', namespace = 'favit')),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^favs/', include('favs.urls', namespace = 'favs'))
-	#,url(r'^.*$', RedirectView.as_view(url='website/', permanent=False), name='index')
  )
 
 
