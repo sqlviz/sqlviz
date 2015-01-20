ae5214bb182a6a5378ba1a6020db51964d477a76
Matthew Feldman <matthew.feldman@gmail.com>
2015-01-19 23:50:25 -0800

    more pep8 issues

--- website/admin.py
+++ website/admin.py
@@ -4,40 +4,45 @@ from django.forms import PasswordInput
 from django_ace import AceWidget
 from models import *
 
+
 class QueryDefaultAdmin(admin.TabularInline):
     model = QueryDefault
 
+
 class QueryPrecedentAdmin(admin.TabularInline):
-    model = QueryPrecedent    
+    model = QueryPrecedent
     fk_name = "final_query"
 
-#class QueryProcessingAdmin(admin.TabularInline):
-#    model = QueryProcessing
 
 class QueryAdminForm(forms.ModelForm):
     class Meta:
         model = Query
-        widgets = {'query_text' : AceWidget(mode='sql')}
+        widgets = {'query_text': AceWidget(mode='sql')}
         exclude = ()
+
 
 class QueryAdmin(admin.ModelAdmin):
     form = QueryAdminForm
-    inlines = [QueryDefaultAdmin, QueryPrecedentAdmin] #, QueryProcessingAdmin]
-    list_display = ('id', 'title', 'db','owner','chart_type','modified_time')
+    inlines = [QueryDefaultAdmin, QueryPrecedentAdmin]
+    list_display = ('id', 'title', 'db', 'owner', 'chart_type', 'modified_time')
     save_as = True
+
 
 class DbAdminForm(forms.ModelForm):
     class Meta:
         model = Db
-        widgets = {'password_encrypted' : PasswordInput()}
+        widgets = {'password_encrypted': PasswordInput()}
         exclude = ()
+
 
 class DbAdmin(admin.ModelAdmin):
     form = DbAdminForm
-    list_display = ('id', 'name_short', 'type','port','username')
+    list_display = ('id', 'name_short', 'type', 'port', 'username')
+
 
 class DashboardQueryAdmin(admin.TabularInline):
     model = DashboardQuery
+
     def get_form(self, request, obj=None, **kwargs):
         form = super(DashboardQueryAdmin, self).get_form(request, obj, **kwargs)
         form.base_fields['order'].initial = 1
@@ -47,7 +52,7 @@ class DashboardQueryAdmin(admin.TabularInline):
 class DashboardAdmin(admin.ModelAdmin):
     model = Dashboard
     inlines = [DashboardQueryAdmin]
-    list_display = ('id', 'title', 'owner','modified_time')
+    list_display = ('id', 'title', 'owner', 'modified_time')
 
 
 admin.site.register(Query, QueryAdmin)
