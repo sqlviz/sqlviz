9c64531c7a215be59f9a011e909571e40e72587c
Trey Hunner <trey@treyhunner.com>
2015-01-23 11:26:52 -0800

    Remove tabs from Python files

--- favs/urls.py
+++ favs/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('favs.views',
-   	url(r'^add-or-remove$', 'add_or_remove',name='add_or_remove'),
-   	url(r'^add$', 'add',name='add'),
+    url(r'^add-or-remove$', 'add_or_remove',name='add_or_remove'),
+    url(r'^add$', 'add',name='add'),
     url(r'^remove$', 'remove', name='remove'),
 )
--- website/urls.py
+++ website/urls.py
@@ -4,13 +4,13 @@ from django.conf.urls import patterns, url
 from views import *
 
 urlpatterns = patterns('',
-   	url(r'^$', index, name='home'),
-   	url(r'^api/query/(?P<query_id>\d+)$', query_api, name='query_api'),
-   	url(r'^query/(?P<query_ids>[,\d]+)$', query_view, name='query'),
-	url(r'^query/(?P<query_names>[,\w ]+)$', query_name, name='query_name'),
-   	url(r'^dashboard/(?P<dashboard_id>\d+)$', dashboard, name='dashboard'),
-   	url(r'^filter/', index, name='filter'),
-   	url(r'^query_interactive/', query_interactive, name='query_interactive'),
-   	url(r'^api/query_interactive/', query_interactive_api, name='query_interactive_api'),
-   	url(r'^api/database_explorer/', database_explorer_api, name='database_explorer_api'),
+   url(r'^$', index, name='home'),
+   url(r'^api/query/(?P<query_id>\d+)$', query_api, name='query_api'),
+   url(r'^query/(?P<query_ids>[,\d]+)$', query_view, name='query'),
+   url(r'^query/(?P<query_names>[,\w ]+)$', query_name, name='query_name'),
+   url(r'^dashboard/(?P<dashboard_id>\d+)$', dashboard, name='dashboard'),
+   url(r'^filter/', index, name='filter'),
+   url(r'^query_interactive/', query_interactive, name='query_interactive'),
+   url(r'^api/query_interactive/', query_interactive_api, name='query_interactive_api'),
+   url(r'^api/database_explorer/', database_explorer_api, name='database_explorer_api'),
 )
