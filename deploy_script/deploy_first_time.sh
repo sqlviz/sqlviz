eb664a7abab2e8ef98420bd1c68d8b1147c9347d
Matthew Feldman <matthew.feldman@gmail.com>
2015-04-05 14:44:57 -0700

    improving deploy script with new instructions

--- deploy_script/deploy_first_time.sh
+++ deploy_script/deploy_first_time.sh
@@ -1,6 +1,5 @@
-sudo yum install mysql-server libmysqlclient-dev python-dev libblas-dev liblapack-dev gfortran
+sudo apt-get install mysql-server libmysqlclient-dev python-dev libblas-dev liblapack-dev gfortran lamp-server^ python-pip python-numpy python-psycopg2  python-psycopg2 libpq-dev libfreetype6-dev libxft-dev phantomjs libxml2-dev libxslt1-dev
 
-pip install -r requirements/base.txt
 pip install -r requirements/local.txt
 
 mysql -u root
@@ -10,20 +9,25 @@ GRANT ALL PRIVILEGES ON django.*  TO 'django'@'localhost' IDENTIFIED BY 'django'
 CREATE DATABASE IF NOT EXISTS scratch CHARACTER SET utf8 COLLATE utf8_general_ci;
 GRANT ALL PRIVILEGES ON scratch.*  TO 'django'@'localhost' IDENTIFIED BY 'django';
 
-
-CREATE DATABASE IF NOT EXISTS test CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON test.*  TO 'django'@'localhost' IDENTIFIED BY 'django';
-
 quit;
 
 ./manage.py migrate
 
 ./manage.py createsuperuser
 
+git clone https://github.com/highslide-software/highcharts.com.git website/static/Highcharts
+git clone https://github.com/highslide-software/highmaps-release.git website/static/Highmaps
+
+rm -rf fieldkeys
 mkdir fieldkeys
 keyczart create --location=fieldkeys --purpose=crypt
 keyczart addkey --location=fieldkeys --status=primary --size=256
 
-./manage.py runserver 7878    
+python manage.py loaddata initial_data/djia_data.json
+python manage.py loaddata initial_data/initial_data.json
 
 chmod  -R 777 media
+
+./manage.py runserver 7878    
+
+
