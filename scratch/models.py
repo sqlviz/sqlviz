b3c40c872786089b9730d2252aa722434ea53a00
Matthew Feldman <matthew.feldman@gmail.com>
2015-04-05 10:16:35 -0700

    adding data to dump

--- scratch/models.py
+++ scratch/models.py
@@ -4,13 +4,3 @@ from django.db import models
 class DimDjia(models.Model):
     date = models.DateTimeField()
     price = models.IntegerField()
-
-
-class DimGdpData(models.Model):
-    code = models.CharField(max_length=128)
-    continent = models.CharField(max_length=128)
-    country = models.CharField(max_length=128)
-    gdp = models.IntegerField()
-    population = models.IntegerField()
-    region = models.CharField(max_length=128)
-    users = models.IntegerField()
