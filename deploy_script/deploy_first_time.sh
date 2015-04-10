4c8d533ee349b0b52c70407b0ca1a88e7b326afb
Matthew Feldman <matthew.feldman@gmail.com>
2015-04-09 22:52:35 -0700

    adding print statements to deploy script

--- deploy_script/deploy_first_time.sh
+++ deploy_script/deploy_first_time.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo apt-get install libmysqlclient-dev python-dev libblas-dev liblapack-dev gfortran lamp-server^ python-pip python-numpy python-psycopg2  python-psycopg2 libpq-dev libfreetype6-dev libxft-dev phantomjs libxml2-dev libxslt1-dev
-
+echo 'apt get complete'
 sudo pip install -r requirements/local.txt
+echo 'pip install complete'
 
-
-## Create passwords randomly
+# Create passwords randomly
 
 MYSQLPWD=$(openssl rand -hex 32)
 DJANGOPWD=$(openssl rand -hex 32)
@@ -41,15 +41,16 @@ PWD_JSON="${PWD_JSON//MYSQLPWD/$MYSQLPWD}"
 
 # Place passwords in necassary files
 echo "$PWD_JSON" > sqlviz/passwords.json
+sed -i "s@MYSQLPWD@$MYSQLPWD@" initial_data/initial_data.json
+
+echo 'please provide mysql root password'
 
 mysql -u root -p -e "CREATE DATABASE IF NOT EXISTS django CHARACTER SET utf8 COLLATE utf8_general_ci;
 	GRANT ALL PRIVILEGES ON django.*  TO 'django'@'localhost' IDENTIFIED BY '$MYSQLPWD';
 	CREATE DATABASE IF NOT EXISTS scratch CHARACTER SET utf8 COLLATE utf8_general_ci;
 	GRANT ALL PRIVILEGES ON scratch.*  TO 'django'@'localhost' IDENTIFIED BY '$MYSQLPWD';"
 
-# Modify  Initial Data to include MySQL Password
-sed -i "s@MYSQLPWD@$MYSQLPWD@" initial_data/initial_data.json
-
+echo 'begin django migrations'
 ./manage.py migrate
 
 ./manage.py createsuperuser
@@ -61,14 +62,17 @@ mkdir fieldkeys
 keyczart create --location=fieldkeys --purpose=crypt
 keyczart addkey --location=fieldkeys --status=primary --size=256
 
+echo 'cloning highcharts. please see licensing details'
+git clone https://github.com/highslide-software/highcharts.com.git website/static/Highcharts
+git clone https://github.com/highslide-software/highmaps-release.git website/static/Highmaps
+
+echo 'loading data into database'
 python manage.py loaddata initial_data/djia_data.json
 python manage.py loaddata initial_data/initial_data.json
 
 chmod  -R 777 media
 
-git clone https://github.com/highslide-software/highcharts.com.git website/static/Highcharts
-git clone https://github.com/highslide-software/highmaps-release.git website/static/Highmaps
-
+echo 'startind dev server'
 ./manage.py runserver 0.0.0.0:8000
 
 
