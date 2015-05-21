368b1c4e50f85474b8544c4a871fe7840e054dec
Matthew Feldman <matthew.feldman@gmail.com>
2015-05-21 10:34:59 -0700

    show tables to fix cirlcei bug

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -276,7 +276,7 @@ class QueryPrecedentTest(QueryAPITestCase):
             title='inner'
         )
         query_outer = QueryFactory(
-            query_text="""use scratch; show tables;"""
+            query_text="""use scratch; show tables;""",
             owner=self.user,
             title='outer',
             db=query_inner.db
