65be099210bbcb13baac79421390de4d3134d04f
Trey Hunner <trey@treyhunner.com>
2015-06-04 16:55:58 -0700

    Remove unused logout confirmation page

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@ from django.contrib.auth.signals import user_logged_out
 from django.contrib import messages
 from django.dispatch import receiver
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     '',
@@ -10,8 +9,6 @@ urlpatterns = patterns(
         'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {
         'next_page': 'accounts:login'}, name='logout'),
-    url(r'^logout_confirm$', TemplateView.as_view(
-        template_name='accounts/logout.html'), name='logout_confirm')
 )
 
 @receiver(user_logged_out)
