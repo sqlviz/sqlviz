53b53f9d2d9844c7674f2bc9aebb270c1c129187
Trey Hunner <trey@treyhunner.com>
2015-06-04 16:54:38 -0700

    Make logout link logout immediately

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import patterns, url, include
+from django.contrib.auth.signals import user_logged_out
+from django.contrib import messages
+from django.dispatch import receiver
+from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 urlpatterns = patterns(
@@ -6,7 +9,11 @@ urlpatterns = patterns(
     url(r'^login$', 'django.contrib.auth.views.login', {
         'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {
-        'next_page': 'accounts:logout_confirm'}, name='logout'),
+        'next_page': 'accounts:login'}, name='logout'),
     url(r'^logout_confirm$', TemplateView.as_view(
         template_name='accounts/logout.html'), name='logout_confirm')
 )
+
+@receiver(user_logged_out)
+def user_logout_handler(sender, request, **kwargs):
+    messages.info(request, "You have logged out.")
