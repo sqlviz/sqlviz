51bf1c7c5d840d5fe8612bc556e477f415280115
Trey Hunner <trey@treyhunner.com>
2015-06-04 16:42:16 -0700

    Fix indentation of accounts urls file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-                       url(r'^login$', 'django.contrib.auth.views.login', {
-                           'template_name': 'accounts/login.html'}, name='login'),
-                       url(r'^logout$', 'django.contrib.auth.views.logout', {
-                           'next_page': 'accounts:logout_confirm'}, name='logout'),
-                       url(r'^logout_confirm$', TemplateView.as_view(
-                           template_name='accounts/logout.html'), name='logout_confirm')
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^login$', 'django.contrib.auth.views.login', {
+        'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {
+        'next_page': 'accounts:logout_confirm'}, name='logout'),
+    url(r'^logout_confirm$', TemplateView.as_view(
+        template_name='accounts/logout.html'), name='logout_confirm')
+)
