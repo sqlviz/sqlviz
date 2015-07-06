bc66d982ca69d1061e3409493f8276fd2de53bdc
Ubuntu <ubuntu@ip-172-31-8-147.us-west-2.compute.internal>
2015-07-06 16:16:29 +0000

    adding dashboards to index. including in requirements

--- website/search_indexes.py
+++ website/search_indexes.py
@@ -1,6 +1,6 @@
 import datetime
 from haystack import indexes
-from models import Query
+from models import Query, Dashboard
 
 
 class QueryIndex(indexes.SearchIndex, indexes.Indexable):
@@ -13,4 +13,21 @@ class QueryIndex(indexes.SearchIndex, indexes.Indexable):
 
     def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return self.get_model().objects.filter(modified_time__lte=datetime.datetime.now())
+        return self.get_model().objects.filter(
+            modified_time__lte=datetime.datetime.now()
+        )
+
+
+class DashboardIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    owner = indexes.CharField(model_attr='owner')
+    modified_time = indexes.DateTimeField(model_attr='modified_time')
+
+    def get_model(self):
+        return Dashboard
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(
+            modified_time__lte=datetime.datetime.now()
+        )
